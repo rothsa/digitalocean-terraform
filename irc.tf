01e7ef3aadb12b82cf6810bf7ad1f51b8dbcbf7e
Sally Lehman <sally.lehman@auth0.com>
2017-03-10 05:23:38 +0000

    add states

---
+++ irc.tf
@@ -0,0 +1,28 @@
+resource "digitalocean_droplet" "irc" {
+    image = ""
+    name = "irc"
+    region = "sfo1"
+    size = "512Mb"
+    private_networking = true
+    ssh_keys = [
+      "${var.ssh_fingerprint}"
+    ]
+
+  connection {
+      user = "root"
+      type = "ssh"
+      key_file = "${var.pvt_key}"
+      timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install blueprint
+      "echo \"deb http://packages.devstructure.com $(lsb_release -sc) main\" | sudo tee /etc/apt/sources.list.d/devstructure.list",
+      "sudo wget -O /etc/apt/trusted.gpg.d/devstructure.gpg http://packages.devstructure.com/keyring.gpg",
+      "sudo apt-get update",
+      "sudo apt-get -y install blueprint"
+    ]
+  }
+}
