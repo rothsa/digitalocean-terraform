c7650762fa54bc0a707f31c4fe31f3f967c73f11
Sally Lehman <sally.lehman@auth0.com>
2017-03-12 05:55:37 +0000

    add master variable

--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@ variable "do_token" {}
 variable "public_key_path" {}
 variable "private_key_path" {}
 variable "ssh_fingerprint" {}
+variable "salt_master" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"
