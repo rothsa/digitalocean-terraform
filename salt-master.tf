0e2295494dbb1a634d5457590f04fc4ac7845dba
Sally Lehman <sally.lehman@oracle.com>
2017-09-30 16:29:51 -0400

    Revert "because my username changes, silly"
    This reverts commit b2e99c8924a29e4acf09863ab21171d84235007c.

---
+++ salt-master.tf
@@ -0,0 +1,24 @@
+resource "digitalocean_droplet" "salt-master" {
+    image = "ubuntu-16-04-x64"
+    name = "salt-master"
+    region = "nyc3"
+    size = "2gb"
+    resize_disk = "false"
+    ssh_keys = ["${digitalocean_ssh_key.salt_key.id}"]
+  
+  connection {
+      user = "root"
+      type = "ssh"
+      private_key = "${file(var.private_key_path)}"
+      timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install salt-master 
+      "curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com",
+      "sudo sh bootstrap-salt.sh -M -N git develop",
+    ]
+  }
+}
