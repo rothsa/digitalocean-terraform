a64c45811c08c7853d5ae0595e40784c8dfb5343
Sally Lehman <sally.lehman@oracle.com>
2017-09-30 16:28:06 -0400

    redo

--- provider.tf
+++ provider.tf
@@ -2,7 +2,6 @@ variable "do_token" {}
 variable "public_key_path" {}
 variable "private_key_path" {}
 variable "ssh_fingerprint" {}
-variable "salt_master" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"
